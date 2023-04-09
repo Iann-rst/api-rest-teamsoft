f791be683d7c7113f2c0e4c093d2d0232e0192cb
Iann Rodrigues <iannrst11@gmail.com>
2023-04-09 20:11:17 -0300

    feat: Rota para  atualizar os dados do Cliente

--- src/http/routes/customer.routes.ts
+++ src/http/routes/customer.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { deleteController } from '../controllers/delete'
 import { register } from '../controllers/register'
 import { search } from '../controllers/search'
+import { update } from '../controllers/update'
 
 export const customerRoutes = Router()
 
@@ -10,3 +11,5 @@ customerRoutes.post('/register', register)
 customerRoutes.delete('/:cnpj/delete', deleteController)
 
 customerRoutes.get('/:cnpj/details', search)
+
+customerRoutes.put('/:cnpj/update', update)
