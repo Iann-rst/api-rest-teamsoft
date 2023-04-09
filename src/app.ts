590d5adcbaed8eb18e906643b2ae2b2fdafb7f13
Iann Rodrigues <iannrst11@gmail.com>
2023-04-09 17:37:07 -0300

    feat: Handler de erros global

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,38 @@
-import express from 'express'
+import express, {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from 'express'
+import 'express-async-errors'
+import { ZodError } from 'zod'
+import { env } from './env'
 import { routes } from './http/routes'
 
 const app = express()
 app.use(express.json())
 app.use(routes)
+
+app.use(
+  (
+    err: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    // Erro do tipo ZodError (validação dos campos: cnpj, razao social, telefone, nome contato, endereços)
+    if (err instanceof ZodError) {
+      return res.status(400).send({
+        message: err.format(),
+      })
+    }
+
+    if (env.NODE_ENV !== 'production') {
+      console.error(err)
+    }
+
+    return res.status(500).send({ message: 'Internal server error.' })
+  },
+)
 
 export { app }
