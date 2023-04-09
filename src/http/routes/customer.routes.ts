ccf684759fa5ea17f3c17c9ce281f7cdc58b1ec2
Iann Rodrigues <iannrst11@gmail.com>
2023-04-09 18:00:15 -0300

    feat: Rota para remover Cliente e seus endereços

--- src/http/routes/customer.routes.ts
+++ src/http/routes/customer.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { deleteController } from '../controllers/delete'
 import { register } from '../controllers/register'
 
 export const customerRoutes = Router()
 
 customerRoutes.post('/register', register)
+
+customerRoutes.delete('/:cnpj/delete', deleteController)
 
 customerRoutes.get('/:cnpj/details', (request, response) => {
   return response.status(200).json({
