1d779452181b6e830ef27a6a95eba552e4252685
Iann Rodrigues <iannrst11@gmail.com>
2023-04-10 09:28:26 -0300

    feat: Métodos do repositório InMemory (Cliente)

--- src/repositories/in-memory/in-memory-customers-repository.ts
+++ src/repositories/in-memory/in-memory-customers-repository.ts
@@ -1,19 +1,64 @@
+import { Customer, Prisma } from '@prisma/client'
 import { CustomersRepository } from '../customers-repository'
 
 export class InMemoryCustomersRepository implements CustomersRepository {
-  async findByCnpj(cnpj: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  public customers: Customer[] = []
+
+  async findByCnpj(cnpj: string): Promise<Customer | null> {
+    const customer = this.customers.find((customer) => customer.cnpj === cnpj)
+
+    if (!customer) {
+      return null
+    }
+
+    return customer
   }
 
-  async findByRazaoSocial(razao_social: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async findByRazaoSocial(razao_social: string): Promise<Customer | null> {
+    const customer = this.customers.find(
+      (customer) => customer.razao_social === razao_social,
+    )
+
+    if (!customer) {
+      return null
+    }
+
+    return customer
   }
 
   async delete(cnpj: string): Promise<void> {
-    throw new Error('Method not implemented.')
+    this.customers.filter((customer) => customer.cnpj !== cnpj)
   }
 
-  async create(data: object): Promise<void> {
-    throw new Error('Method not implemented.')
+  async create(data: Prisma.CustomerCreateInput): Promise<Customer> {
+    const customer = {
+      cnpj: data.cnpj,
+      razao_social: data.razao_social,
+      nome_contato: data.nome_contato,
+      tel: data.tel,
+    }
+
+    this.customers.push(customer)
+
+    return customer
+  }
+
+  async update(
+    cnpj: string,
+    data: Prisma.CustomerUpdateInput,
+  ): Promise<Customer> {
+    const customerIndex = this.customers.findIndex(
+      (customer) => customer.cnpj === cnpj,
+    )
+
+    const customer = {
+      cnpj,
+      razao_social: data.razao_social as string,
+      nome_contato: data.nome_contato as string,
+      tel: data.tel as string,
+    }
+    this.customers[customerIndex] = customer
+
+    return this.customers[customerIndex]
   }
 }
