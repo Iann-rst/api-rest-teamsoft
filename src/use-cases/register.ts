4f02ce619168d4940b54035cd83a1c8b2ae1a03a
Iann Rodrigues <iannrst11@gmail.com>
2023-04-10 09:28:56 -0300

    fix: Retorno do método de registrar Cliente

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,8 +1,14 @@
 import { Props } from '@/http/controllers/register'
 import { AddressesRepository } from '@/repositories/addresses-repository'
 import { CustomersRepository } from '@/repositories/customers-repository'
+import { Customer } from '@prisma/client'
 import { CustomerWithCnpjAlreadyExists } from './erros/customer-with-cnpj-already-exists'
 import { CustomerWithCorporateNameAlreadyExists } from './erros/customer-with-corporate-name-already-exists'
+
+interface RegisterUseCaseResponse {
+  customer: Customer
+  number_of_addressed_created: number
+}
 
 export class RegisterUseCase {
   constructor(
@@ -10,7 +16,13 @@ export class RegisterUseCase {
     private addressesRepository: AddressesRepository,
   ) {} // repository de addresses // repository de customers
 
-  async execute({ cnpj, nome_contato, razao_social, tel, addresses }: Props) {
+  async execute({
+    cnpj,
+    nome_contato,
+    razao_social,
+    tel,
+    addresses,
+  }: Props): Promise<RegisterUseCaseResponse> {
     // Não deve ser possível criar cliente que tenha o mesmo cnpj
     // Não deve ser possível criar cliente que tenho a mesma razao_social
 
@@ -32,16 +44,16 @@ export class RegisterUseCase {
       tel,
     })
 
-    if (customer) {
-      // Apos criar cliente, adicionar os endereços na tabela de endereço. Passando o cnpj do cliente
+    // Apos criar cliente, adicionar os endereços na tabela de endereço. Passando o cnpj do cliente
 
-      // Adicionar o cnpj do cliente para cada endereço (chave estrangeira da tabela endereço)
-      const new_data = addresses.map((address) => {
-        return { ...address, customer_cnpj: customer.cnpj }
-      })
-      await this.addressesRepository.create(new_data)
-    } else {
-      throw new Error('Não foi possível cadastrar o cliente')
-    }
+    // Adicionar o cnpj do cliente para cada endereço (chave estrangeira da tabela endereço)
+    const new_data = addresses.map((address) => {
+      return { ...address, customer_cnpj: customer.cnpj }
+    })
+    const number_of_addressed_created = await this.addressesRepository.create(
+      new_data,
+    )
+
+    return { customer, number_of_addressed_created }
   }
 }
