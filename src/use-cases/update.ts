813b38f6372b51b69ae1ed8a126369711424a927
Iann Rodrigues <iannrst11@gmail.com>
2023-04-10 12:43:15 -0300

    fix: UpdateUseCase

--- src/use-cases/update.ts
+++ src/use-cases/update.ts
@@ -1,32 +1,36 @@
-import { IPropsUpdate } from '@/http/controllers/update'
 import { CustomersRepository } from '@/repositories/customers-repository'
+import { Customer } from '@prisma/client'
 import { CustomerNotFound } from './erros/customer-not-found'
-import { CustomerWithCorporateNameAlreadyExists } from './erros/customer-with-corporate-name-already-exists'
+
+interface ResponseUpdateUseCase {
+  updated_customer: Customer
+}
+
+interface RequestUpdateUseCase {
+  cnpj: string
+  nome_contato?: string
+  tel?: string
+}
 
 export class UpdateUseCase {
   constructor(private customersRepository: CustomersRepository) {}
 
-  async execute({ cnpj, nome_contato, razao_social, tel }: IPropsUpdate) {
+  async execute({
+    cnpj,
+    nome_contato,
+    tel,
+  }: RequestUpdateUseCase): Promise<ResponseUpdateUseCase> {
     const customer = await this.customersRepository.findByCnpj(cnpj)
 
     if (!customer) {
       throw new CustomerNotFound()
     }
-    // Não deve ser possível atualizar a razão social para uma que ja existe
-
-    const customerWithSameCorporateName =
-      await this.customersRepository.findByRazaoSocial(razao_social)
-
-    if (customerWithSameCorporateName) {
-      throw new CustomerWithCorporateNameAlreadyExists()
-    }
 
     const updated_customer = await this.customersRepository.update(cnpj, {
       nome_contato,
-      razao_social,
       tel,
     })
 
-    return updated_customer
+    return { updated_customer }
   }
 }
